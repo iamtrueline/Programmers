383361face863230069b13b9db2de5af74697509
Jinseon KANG <iamtrueline@gmail.com>
2021-07-22 17:23:24 +0900

    Create 문자열 내 p와 y의 개수.py

---
+++ Python/문자열 내 p와 y의 개수.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = True
+    p = 0
+    y = 0
+    for i in range(len(s)):
+        if s[i] == 'p' or s[i] == 'P':
+            p += 1
+        elif s[i] == 'y' or s[i] == 'Y':
+            y += 1
+    if p==y:
+        answer = True
+    else:
+        answer = False
+    return answer
